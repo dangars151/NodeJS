edc224f92d7e07957566030f8ad15702fe36f495
Dang JR <hathedang@gmail.com>
2020-04-05 00:52:32 +0700

    Giai quyet duoc bo luu ma khong can cau hinh lai roi nay hehehe

--- do-an/app/controllers/cancel-save-job.js
+++ do-an/app/controllers/cancel-save-job.js
@@ -3,9 +3,21 @@ var express = require("express");
 var router = express.Router();
 
 var job = require('../models/Job');
+var user = require('../models/User');
 
-router.get('/:id', function(req, res) {
-    job.find({_id: {$in: req.session.user.jobsId}}).then(data => {
+router.post('/', function(req, res) {
+    var jobsId = req.session.user.jobsId;
+    for( var i = 0; i < jobsId.length; i++){ 
+        if ( jobsId[i] == req.body.jobId) {
+            jobsId.splice(i, 1); 
+        }
+    }
+    req.session.user.jobsId = jobsId;
+    user.updateOne({_id: req.session.user._id}, {
+        jobsId: jobsId
+    }).then(data => {
+        return job.find({_id: {$in: jobsId}})
+    }).then(data => {
         return res.render('save-job', {jobs: data, data: req.session.user});
     })
 });
