6b131b0c5b10b1b90bb47f542c5a41e344f91af7
Dang JR <hathedang@gmail.com>
2020-04-13 00:18:14 +0700

    Code them tinh nang apply-cv

--- do-an/app/controllers/route.js
+++ do-an/app/controllers/route.js
@@ -27,6 +27,5 @@ router.use('/job-detail', require(__dirname + '/job-detail'));
 router.use('/cancel-save-job', require(__dirname + '/cancel-save-job'));
 router.use('/handle-notification', require(__dirname + '/handle-notification'));
 router.use('/apply-cv', require(__dirname + '/apply-cv'));
-router.use('/cv-of-job', require(__dirname + '/cv-of-job'));
 
 module.exports = router;
